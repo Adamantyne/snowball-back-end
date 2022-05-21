de9895436122d4795ad79de36b96d5a6f33660b9
Guilherme Alves <guialvest@gmail.com>
2022-05-20 21:18:59 -0300

    put videos

--- src/routes/videosRouter.js
+++ src/routes/videosRouter.js
@@ -11,6 +11,22 @@ videosRouter.get("/videos/:id", validateVideoId, getVideo);
 
 videosRouter.post("/videos", validateVideo, createVideo);
 
-
+videosRouter.put("/videos", (req,res=>{
+    const {video,question,email} = req.body;
+    try {
+        const collection = db.collection("videos");
+        await collection.updateOne(video._id,{$set:{
+            questions:[...video.questions,{
+                question,
+                answer,
+                email
+            }]
+        }});
+    } catch (error) {
+        console.log("Error logging out.");
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}));
 
 export default videosRouter;
